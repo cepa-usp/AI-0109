e4b75a8b3d8daaa510cf818632ca590e568a57be
Ivan Ramos Pagnossin <irpagnossin.edu@gmail.com>
2013-10-18 13:18:23 -0300

    Planilha de interações de cor: procurando o porquê de o mundo virtual estabilizar com a cor azul.

--- simulation/Interactions.py
+++ simulation/Interactions.py
@@ -9,6 +9,12 @@ def pretty_print(p1in, p2in, p1out, p2out, attr = None):
     else:        
         print FORMAT.format(p1in, p2in, p1out, p2out)
     
+def wrap(x):
+    if x == -1: return 3
+    if x == -2: return 2
+    if x == -3: return 1
+    return x
+    
 p1in = Particle(Color.BLUE, Shape.CIRCLE, Symbol.HEART)
 p2in = Particle(Color.RED, Shape.SQUARE, Symbol.SPADE)
 (p1out, p2out) = p1in.collide(p2in)
@@ -17,3 +23,15 @@ pretty_print(p1in, p2in, p1out, p2out)
 #pretty_print(p1in, p2in, p1out, p2out, attr = Color)
 #pretty_print(p1in, p2in, p1out, p2out, attr = Shape)
 #pretty_print(p1in, p2in, p1out, p2out, attr = Symbol)
+
+line = ["\t0\t1\t2\t3\n"]
+for x in range(4):
+    line.append("%d\t" % x)
+    p1in = Particle(x, Shape.CIRCLE, Symbol.HEART)
+    for y in range(4):        
+        p2in = Particle(y, Shape.CIRCLE, Symbol.HEART)
+        (p1out, p2out) = p1in.collide(p2in)
+        line.append("%d\t" % wrap(p1out.color))
+    line.append("\n")
+    
+print "".join(line)
